b4d7b5fb101013574e33490a2728776a298c171c
Rajdeep <89543670+rrajdeep@users.noreply.github.com>
2023-05-08 10:59:23 +0530

    Update linechart.jsx

--- src/components/linechart.jsx
+++ src/components/linechart.jsx
@@ -1,45 +1,53 @@
 import React from "react";
 import { Line } from "react-chartjs-2";
 import {
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend,
-  } from 'chart.js';
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
 
-  ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend
-  );
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
-const LineChart = ({chartData}) => {
-    
-    const labels = chartData.map(ele => ele.x );
-    const data = {
-      labels: labels,
-      datasets: [
-        {
-          label: "value of x",
-          backgroundColor: 'rgba(255, 99, 132, 1)',
-          borderColor: 'rgba(255,99,132,0.5)',
-          data: chartData,
-        },
-      ],
-    };
+const LineChart = ({ chartData }) => {
+  const labels = chartData.map((ele) => ele.x);
+  const data = {
+    labels: labels,
+    datasets: [
+      {
+        label: "value of x",
+        backgroundColor: "rgba(255, 99, 132, 1)",
+        borderColor: "rgba(255,99,132,0.5)",
+        data: chartData,
+      },
+    ],
+  };
 
+  const options = {
+    plugins: {
+      title: {
+        display: true,
+        text: "Line chart",
+        align: "center",
+      },
+    },
+  };
 
   return (
     <div>
-      <Line data={data} />
+      <Line options={options} data={data} />
     </div>
   );
 };
